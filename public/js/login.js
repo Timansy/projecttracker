b1919fa69cea73df7cb8dc7fe73e08a8cf26ba33
Your Name <danygrimaldo@gmail.com>
2020-06-05 22:37:20 -0400

    intorduced css and created logindg page

--- public/js/login.js
+++ public/js/login.js
@@ -1,9 +1,1 @@
-const router = require("express").Router();
-
-router.post("/register", (req, res) => {
-  res.send("Register");
-});
-
-// router.post("/login");
-
-module.exports = router;
+html5;
