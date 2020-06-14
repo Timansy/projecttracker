c14bb017a9375810795f5b098a3448275531fd20
John Herman <hermanjohn2@gmail.com>
2020-06-14 12:44:35 -0400

    Added bCrypt password hashing to protect user password in DB. The users password will now be encrypted upon registering or being created by an admin. Tested by registering admin, creating user as admin and logging in with both. Verified passwords are encrypted by checking User table in DB.

--- models/user.js
+++ models/user.js
@@ -1,24 +1,38 @@
+var bcrypt = require('bcryptjs');
+
 // Creating our User model
 module.exports = function (sequelize, DataTypes) {
-  var User = sequelize.define("User", {
-    username: DataTypes.STRING,
-    password: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    auth_level: {
-      type: DataTypes.STRING,
-      isIn: [["administrator", "project_manager", "developer"]],
-      defaultValue: "administrator"
-    },
-  });
+    var User = sequelize.define('User', {
+        username: DataTypes.STRING,
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        auth_level: {
+            type: DataTypes.STRING,
+            isIn: [['administrator', 'project_manager', 'developer']],
+            defaultValue: 'administrator'
+        }
+    });
 
-  User.associate = (models) => {
-    User.hasMany(models.Project, {});
-    User.hasMany(models.Task, {});
-  };
+    User.associate = (models) => {
+        User.hasMany(models.Project, {});
+        User.hasMany(models.Task, {});
+    };
 
-  //could add bcrypt stuff??
+    // Compares the password entered with the bCrypt comparison method
+    User.prototype.validPassword = (userPassword, password) => {
+        return bcrypt.compareSync(password, userPassword);
+    };
 
-  return User;
+    // Hashes a user's password before the user is created
+    User.beforeCreate((user) => {
+        user.password = bcrypt.hashSync(
+            user.password,
+            bcrypt.genSaltSync(10),
+            null
+        );
+    });
+
+    return User;
 };
