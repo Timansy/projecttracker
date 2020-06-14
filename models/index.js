f7ddf71281873d9721566f9ca9ae2e185f3c577e
Timansy <brown.tim@gmail.com>
2020-06-14 16:33:15 -0400

    heroku set-up 5

--- models/index.js
+++ models/index.js
@@ -9,7 +9,7 @@ const config = require(__dirname + '/../config/config.json')[env];
 const db = {};
 
 let sequelize;
-if (config.use_env_variable) {
+if (process.env.JAWSDB_URL) {
   console.log("aaa");
   sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
