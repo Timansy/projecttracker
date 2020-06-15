66dcd0e9789580dcffc6da40291f5701bb783668
Andrew Reeves <andrew.reeves.coding.bootcamp@gmail.com>
2020-06-14 22:41:38 -0400

    task/phase model corrected, phases delete cascade

--- models/projectphase.js
+++ models/projectphase.js
@@ -1,27 +1,25 @@
-module.exports = function (sequelize, DataTypes) {
-  var ProjectPhase = sequelize.define("ProjectPhase", {
-    title: DataTypes.STRING,
-    phaseComplete: {
-      type: DataTypes.BOOLEAN,
-      defaultValue: false,
-    },
-  });
-
-  
-
-  ProjectPhase.associate = (models) => {
-    ProjectPhase.hasMany(models.Task, {
-      onDelete: "cascade",
+module.exports = function(sequelize, DataTypes) {
+    var ProjectPhase = sequelize.define("ProjectPhase", {
+        title: DataTypes.STRING,
+        phaseComplete: {
+            type: DataTypes.BOOLEAN,
+            defaultValue: false,
+        },
     });
-  };
 
-  ProjectPhase.associate = (models) => {
-    ProjectPhase.belongsTo(models.Project,{
-      foreignKey: {
-        allowNull: false,
-      },
-    });
-  };
 
-  return ProjectPhase;
+
+    ProjectPhase.associate = (models) => {
+        ProjectPhase.hasMany(models.Task);
+    };
+
+    ProjectPhase.associate = (models) => {
+        ProjectPhase.belongsTo(models.Project, {
+            foreignKey: {
+                allowNull: false,
+            },
+        });
+    };
+
+    return ProjectPhase;
 };
--- models/task.js
+++ models/task.js
@@ -1,24 +1,23 @@
-module.exports = function (sequelize, DataTypes) {
-  var Task = sequelize.define("Task", {
-    taskname: DataTypes.STRING,
-    isComplete: DataTypes.BOOLEAN,
-  });
-
-  Task.associate = (models) => {
-    Task.belongsTo(models.User, {
-      foreignKey: {
-        allowNull: true,
-      },
+module.exports = function(sequelize, DataTypes) {
+    var Task = sequelize.define("Task", {
+        taskname: DataTypes.STRING,
+        isComplete: DataTypes.BOOLEAN,
     });
-  };
 
-  Task.associate = (models) => {
-    Task.belongsTo(models.ProjectPhase, {
-      foreignKey: {
-        allowNull: false,
-      },
-    });
-  };
+    Task.associate = (models) => {
+        Task.belongsTo(models.User, {
+            foreignKey: {
+                allowNull: true,
+            },
+        });
+    };
 
-  return Task;
+    Task.associate = (models) => {
+        Task.belongsTo(models.ProjectPhase, {
+            foreignKeyConstraint: true,
+            onDelete: "CASCADE"
+        });
+    };
+
+    return Task;
 };
